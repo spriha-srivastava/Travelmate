43ed8d990fdd9b294fdcdf0070087c31cc7251ec
Spriha Srivastava <srivastavaspriha18@gmail.com>
2025-12-16 01:20:45 +0530

    add slider

--- components/Home/Reviews/ReviewSlider.tsx
+++ components/Home/Reviews/ReviewSlider.tsx
@@ -6,11 +6,26 @@ import  'Swiper/css';
 import "swiper/css/effect-cards";
 
 import{EffectCards} from "swiper/modules"
+import { effectInit } from 'swiper/effect-utils';
+import { reviewData } from '@/data/data';
 
 
 const ReviewSlider = () => {
   return (
-    <div>ReviewSlider</div>
+    <div>
+      <Swiper effect={'cards'} grabCursor={true} modules={[EffectCards]} className='md:w-[450px] md:h-[350px] w-[90%] h-[300px]' >
+        {reviewData.map((data) =>{
+          return(
+            <SwiperSlide key={data.id} className='bg-white rounded-3xl block'>
+               <div className='w-[80%] mx-auto mt-16'>
+                <p className='text-xs sm:text-sm md:text-base font-semibold'>{data.review}</p>
+               </div>
+            </SwiperSlide>
+          )
+        })}
+
+      </Swiper>
+    </div>
   )
 }
 
