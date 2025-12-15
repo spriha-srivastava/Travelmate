222b983fdd4144651175142e591ff23b6aefd15a
Spriha Srivastava <srivastavaspriha18@gmail.com>
2025-12-16 00:50:39 +0530

    add reviews

---
+++ components/Home/Reviews/ReviewSlider.tsx
@@ -0,0 +1,17 @@
+'use client'
+import React from 'react'
+
+import {Swiper,SwiperSlide} from 'swiper/react';
+import  'Swiper/css';
+import "swiper/css/effect-cards";
+
+import{EffectCards} from "swiper/modules"
+
+
+const ReviewSlider = () => {
+  return (
+    <div>ReviewSlider</div>
+  )
+}
+
+export default ReviewSlider
