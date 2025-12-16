9fb85d2877f0adadfa29a303f4134571bf546cf6
spriha-srivastava <srivastavaspriha18@gmail.com>
2025-12-16 13:01:28 +0530

    issues

--- components/Home/Reviews/ReviewSlider.tsx
+++ components/Home/Reviews/ReviewSlider.tsx
@@ -1,25 +1,57 @@
 'use client'
 import React from 'react'
 
-import {Swiper,SwiperSlide} from 'swiper/react';
-import  'Swiper/css';
-import "swiper/css/effect-cards";
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/bundle'
 
-import{EffectCards} from "swiper/modules"
-import { effectInit } from 'swiper/effect-utils';
-import { reviewData } from '@/data/data';
+
+import { EffectCards } from 'swiper/modules'
+import Image from 'next/image'
+import { reviewData } from '@/data/data'
+import { FaStar } from 'react-icons/fa'
 
 
 const ReviewSlider = () => {
   return (
     <div>
-      <Swiper effect={'cards'} grabCursor={true} modules={[EffectCards]} className='md:w-[450px] md:h-[350px] w-[90%] h-[300px]' >
-        {reviewData.map((data) =>{
-          return(
+      <Swiper effect={'cards'}
+        grabCursor={true}
+        modules={[EffectCards]}
+        className='md:w-[450px] md:h-[350px] w-[90%] h-[300px]' >
+        {reviewData.map((data) => {
+          return (
             <SwiperSlide key={data.id} className='bg-white rounded-3xl block'>
-               <div className='w-[80%] mx-auto mt-16'>
+              <div className='w-[80%] mx-auto mt-16'>
                 <p className='text-xs sm:text-sm md:text-base font-semibold'>{data.review}</p>
-               </div>
+
+
+                {/*icons*/}
+                <div className='flex items-center mt-4'>
+                  <FaStar className='md:w-6 md:h-6 w-3 h-3 text-yellow-600' />
+                  <FaStar className='md:w-6 md:h-6 w-3 h-3 text-yellow-600' />
+                  <FaStar className='md:w-6 md:h-6 w-3 h-3 text-yellow-600' />
+                  <FaStar className='md:w-6 md:h-6 w-3 h-3 text-yellow-600' />
+                  <FaStar className='md:w-6 md:h-6 w-3 h-3 text-yellow-600' />
+                </div>
+
+
+                {/*Users*/}
+                <div>
+                  <div>
+                    <Image src={data.image}
+                      width={60}
+                      height={60}
+                      alt='client'
+                      className='rounded-full'
+                    />
+                  </div>
+                  <div>
+                    <p className='text-sm sm:text-lg font-semibold'>{data.name}</p>
+                    <p className='text-gray-600 text-xs sm:text-base'>Web Developer</p>
+                  </div>
+                </div>
+              </div>
             </SwiperSlide>
           )
         })}
