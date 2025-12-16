652a4f25b70cdd96790d23151211221184849964
Spriha Srivastava <srivastavaspriha18@gmail.com>
2025-12-16 20:53:12 +0530

    something

---
+++ components/Home/News/News.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const News = () => {
+    return (
+        <div className='w-full h-full'>
+
+        </div>
+    )
+}
+
+export default News
--- components/Home/Reviews/ReviewSlider.tsx
+++ components/Home/Reviews/ReviewSlider.tsx
@@ -1,15 +1,16 @@
 'use client'
 import React from 'react'
 
-import { Swiper, SwiperSlide } from 'swiper/react'
-import 'swiper/css'
-import 'swiper/css/bundle'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'Swiper/css';
+import "swiper/css/effect-cards";
+import "swiper/css/pagination";
+import { FaStar } from "react-icons/fa";
+import Image from "next/image";
 
-
-import { EffectCards } from 'swiper/modules'
-import Image from 'next/image'
-import { reviewData } from '@/data/data'
-import { FaStar } from 'react-icons/fa'
+import { EffectCards } from "swiper/modules"
+import { effectInit } from 'swiper/effect-utils';
+import { reviewData } from '@/data/data';
 
 
 const ReviewSlider = () => {
